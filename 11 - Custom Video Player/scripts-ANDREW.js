1302f9c023efc805e2024b17d3173dfa99e38897
Andrew De Abreu Ruiz <andrewabreur@gmail.com>
2019-12-20 15:06:27 +0000

    Complete Day 11 - Custom HTML5 Video Player

---
+++ 11 - Custom Video Player/scripts-ANDREW.js
@@ -0,0 +1,86 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const fullscreen = player.querySelector('.fullscreen');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+function openFullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.mozRequestFullScreen) { 
+        video.mozRequestFullScreen();
+      } else if (video.webkitRequestFullscreen) { 
+        video.webkitRequestFullscreen();
+      } else if (video.msRequestFullscreen) { 
+        video.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) { 
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) { 
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { 
+        document.msExitFullscreen();
+    }   
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+video.addEventListener('dblclick', openFullscreen);
+video.addEventListener('dblclick', closeFullscreen);
+
+toggle.addEventListener('click', togglePlay);
+
+fullscreen.addEventListener('click', openFullscreen);
+
+skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
